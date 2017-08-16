385fd42bb52a384e39dfa2ff226abd5cd3fb27ee
milagrosMndoza <milagrosmndoza@gmail.com>
2017-08-16 10:14:22 -0500

    Proyecto Terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,68 @@
-var galeria= document.getElementById("pictures");
-var origen = document.getElementById("btnOrigen");
-var extincion= document.getElementById("btnExtincion");
-var restaurar= document.getElementById("btnRestaurar");
-/*var resTexto= document.getElementById("btnResTexto");*/
-var izquierda= document.getElementById("caja_izq");
-var derecha= document.getElementById("caja_der");
+    var origen = document.getElementById("btnOrigen");
+    var extincion= document.getElementById("btnExtincion");
+    var restaurar= document.getElementById("btnRestaurar");
+    var izquierda= document.getElementById("caja_izq");
+    var derecha= document.getElementById("caja_der");
+
+    var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+    var section = document.getElementById('pictures');
+
+    function imagen(){
+        for (var i = 0; i <pictures.length; i++) {
+            var imagen="img"+i;
+        var divPhoto = document.createElement("div");
+            divPhoto.setAttribute("id", imagen);
+        var photo = document.createElement('img');
+        photo.setAttribute('src', pictures[i]);
+        photo.setAttribute('class', 'foto');
+        divPhoto.appendChild(photo);
+        section.appendChild(divPhoto);
+
+        var xbutton = document.createElement('button');
+        xbutton.setAttribute('class', 'xBtn');
+        xbutton.setAttribute('onclick', 'borrar(this)');
+        var x = document.createTextNode("X");//es lo que me aparecera en el buton X
+        xbutton.appendChild(x);
+        divPhoto.appendChild(xbutton);
+        }
+    }
+    imagen();
+
+    restaurar.addEventListener("click", restaurarFoto);
+    function restaurarFoto(){
+        var divImagenes= document.getElementById("img0");
+        var divImagenes1= document.getElementById("img1");
+        var divImagenes2= document.getElementById("img2");
+        var divImagenes3= document.getElementById("img3");
+       if(divImagenes.style.display === "none"||divImagenes1.style.display === "none"||divImagenes2.style.display === "none" ||divImagenes3.style.display === "none"){
+           divImagenes.style.display = "inline-block";
+           divImagenes1.style.display = "inline-block";
+           divImagenes2.style.display = "inline-block";
+           divImagenes3.style.display = "inline-block";
+       }
+        
+        /*imagen();*/
+    }
+  
+    function borrar(d){
+        d.parentNode.style.display = "none";
+    }
 
 
-
-var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
-var section = document.getElementById('pictures');
-
- for (var i = 0; i < pictures.length; i++) {
-  var divPhoto = document.createElement("div");
-  var photo = document.createElement('img');
-  photo.setAttribute('src', pictures[i]);
-  photo.setAttribute('class', 'foto');
-  divPhoto.appendChild(photo);
-  section.appendChild(divPhoto);
-
-  var xbutton = document.createElement('button');
-  divPhoto.appendChild(xbutton);
-  xbutton.textContent= "X";
-  xbutton.setAttribute('class', 'xBtn');
-
-  xbutton.setAttribute("onclick", "borrar(this)");
-}
-/*
-$(document).ready(function(){
-    $(".xBtn").click(function(){
-        $(".foto").toggle();
+    origen.addEventListener("click", function(){
+    caja_izq.classList.toggle("show");
     });
-});
-*/
-function borrar(){
-    xbutton.parentNode.style.display=none;
-}
-
-    
-origen.addEventListener("click", function(){
-
-caja_izq.classList.toggle("show");
-
-});
 
 
-extincion.addEventListener("click", function(){
-caja_der.classList.toggle("show2");
-});
+    extincion.addEventListener("click", function(){
+    caja_der.classList.toggle("show2");
+    });
 
-/*resTexto.addEventListener("click", function(){
-caja_der.classList.toggle("show2");
-});*/
 
-//position absolute
-//z-index
-//margin
-
- 
+  /*
+    $(document).ready(function(){
+        $(".xBtn").click(function(){
+            $(".foto").toggle();
+        });
+    });
+    */
