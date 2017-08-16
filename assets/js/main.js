95eff70b223f54f9c24e541da282cd3a130f8af1
milagrosMndoza <milagrosmndoza@gmail.com>
2017-08-16 10:22:10 -0500

    Proyecto terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//Enlazo los Ids de mi html con las neuvas vaariables que le asignaré con mi js
     var origen = document.getElementById("btnOrigen");
     var extincion= document.getElementById("btnExtincion");
     var restaurar= document.getElementById("btnRestaurar");
@@ -6,28 +7,29 @@
 
     var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
     var section = document.getElementById('pictures');
-
+//Función para que aparezcan mis imagenes pandas en mi pagina
     function imagen(){
         for (var i = 0; i <pictures.length; i++) {
             var imagen="img"+i;
         var divPhoto = document.createElement("div");
             divPhoto.setAttribute("id", imagen);
         var photo = document.createElement('img');
-        photo.setAttribute('src', pictures[i]);
-        photo.setAttribute('class', 'foto');
-        divPhoto.appendChild(photo);
-        section.appendChild(divPhoto);
+            photo.setAttribute('src', pictures[i]);
+            photo.setAttribute('class', 'foto');
+            divPhoto.appendChild(photo);
+            section.appendChild(divPhoto);
 
         var xbutton = document.createElement('button');
-        xbutton.setAttribute('class', 'xBtn');
-        xbutton.setAttribute('onclick', 'borrar(this)');
+            xbutton.setAttribute('class', 'xBtn');
+            xbutton.setAttribute('onclick', 'borrar(this)');
         var x = document.createTextNode("X");//es lo que me aparecera en el buton X
-        xbutton.appendChild(x);
-        divPhoto.appendChild(xbutton);
+            xbutton.appendChild(x);
+            divPhoto.appendChild(xbutton);
         }
     }
     imagen();
 
+    //Función para restaurar mis fotos pandas
     restaurar.addEventListener("click", restaurarFoto);
     function restaurarFoto(){
         var divImagenes= document.getElementById("img0");
@@ -44,16 +46,17 @@
         /*imagen();*/
     }
   
+    //Función para borrar o desaparecer las fotos de panda
     function borrar(d){
         d.parentNode.style.display = "none";
     }
 
-
+    //Funcion para que me desaparezca y aparezca el texto de la izquierda
     origen.addEventListener("click", function(){
     caja_izq.classList.toggle("show");
     });
 
-
+    //Funcion para que me desaparezca y aparezca el texto de la derecha
     extincion.addEventListener("click", function(){
     caja_der.classList.toggle("show2");
     });
